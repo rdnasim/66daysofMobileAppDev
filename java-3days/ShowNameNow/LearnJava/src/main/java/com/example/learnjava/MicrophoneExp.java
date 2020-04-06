927e49b2b962c2e324318b36f081aadc7470899b
Riadul Islam Nasim <rdislamnasim78@gmail.com>
2020-04-06 12:39:54 +0600

    updated

---
+++ java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MicrophoneExp.java
@@ -0,0 +1,7 @@
+package com.example.learnjava;
+
+public class MicrophoneExp {
+    String name;
+    String color;
+    int model;
+}
---
+++ java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClassExp.java
@@ -0,0 +1,12 @@
+package com.example.learnjava;
+
+public class MyClassExp {
+    public static void main(String[] args) {
+        MicrophoneExp microphoneExp = new MicrophoneExp();
+        microphoneExp.name = "Agenda";
+        microphoneExp.color = "Black";
+        microphoneExp.model = 11772;
+
+        System.out.println(microphoneExp);
+    }
+}
