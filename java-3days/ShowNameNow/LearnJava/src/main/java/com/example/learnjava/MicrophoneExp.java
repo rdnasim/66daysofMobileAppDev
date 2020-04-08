4574dab8353f2e12482f6f0e87c9f56361b7a5a6
Riadul Islam Nasim <rdislamnasim78@gmail.com>
2020-04-08 21:15:19 +0600

    update MyClassExp and MicrophoneExp

--- java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MicrophoneExp.java
+++ java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MicrophoneExp.java
@@ -4,4 +4,53 @@ public class MicrophoneExp {
     String name;
     String color;
     int model;
+
+    public MicrophoneExp(){
+
+    }
+
+    public MicrophoneExp(String name, String color, int model) {
+        this.name = name;
+        this.color = color;
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    public void turnOff(){
+        System.out.println(this.name + " Turn Off.");
+    }
+    public void turnOn(){
+        System.out.println(this.name + " Turn On.");
+    }
+    public void setVolume(){
+        System.out.println(this.name + " Setting volume.");
+    }
+
+    public String showDescription(){
+        return "Microphone name: " + this.name + ", model: " + this.model + ", color: "
+            + this.color;
+    }
 }
--- java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClassExp.java
+++ java-3days/ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClassExp.java
@@ -2,11 +2,32 @@ package com.example.learnjava;
 
 public class MyClassExp {
     public static void main(String[] args) {
-        MicrophoneExp microphoneExp = new MicrophoneExp();
-        microphoneExp.name = "Agenda";
-        microphoneExp.color = "Black";
-        microphoneExp.model = 11772;
+        MicrophoneExp microphoneExp = new MicrophoneExp("Agenda", "Black", 11772);
+//        microphoneExp.name = "Agenda";
+//        microphoneExp.color = "Black";
+//        microphoneExp.model = 11772;
 
-        System.out.println(microphoneExp);
+        microphoneExp.setModel(77112);
+        microphoneExp.setColor("Deep Black");
+        microphoneExp.setName("New Agenda");
+
+        System.out.println("New Mic: " + microphoneExp.getModel());
+
+        MicrophoneExp gHMic = new MicrophoneExp("gHMic", "Red", 73294);
+//        gHMic.name = "gHMic";
+//        gHMic.color = "Red";
+//        gHMic.model = 73294;
+
+//        System.out.println("Name: " + microphoneExp.name + ", " + "Color: " + microphoneExp.color);
+
+        microphoneExp.turnOn();
+        microphoneExp.turnOff();
+        microphoneExp.setVolume();
+
+        System.out.println(microphoneExp.showDescription());
+
+        MicrophoneExp newMic = new MicrophoneExp("NewMic", "Green", 721);
     }
+
 }
+
